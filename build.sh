e7c9e412b4a3b1f4f538238d8918c4e1653ce2e4
Matt Shilling <mattthewshilling@gmail.com>
2022-07-24 23:56:52 -0700

    Adding explanation for bad build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# ST32 Cube Programmer expects the executables to have a file extension.
+# We are building ELF executables.
+# - cargo-readobj target/thumbv7em-none-eabihf/debug/target -- --file-headers
+# Add the .elf suffix.
+# We could probably load the file via ST32 cube CLI or something.
 rm -f target/thumbv7em-none-eabihf/debug/target.elf
 cargo build
 mv -f target/thumbv7em-none-eabihf/debug/target \
